77d2037fac640e52ace2226b0f367f5523a32402
Yukihiko Saito <y-saito@safie.jp>
2020-03-18 16:24:44 +0900

    パスパラメータとクエリパラメータのチュートリアルやった https://fastapi.tiangolo.com/tutorial/path-params/ https://fastapi.tiangolo.com/tutorial/query-params/

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+# FastAPIはStarletteから直接継承するクラスです
+# Starletteのすべての機能を利用出来ます
 from fastapi import FastAPI
+from enum import Enum
+
+from typing import Optional
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -9,5 +21,26 @@ async def read_root():
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int, limit: Optional[int] = None):
+    return {"item_id": item_id, "limit": limit}
+
+
+# パスの操作は順番に評価されます / 順番に注意
+# @see https://fastapi.tiangolo.com/tutorial/path-params/
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
