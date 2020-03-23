3b9faf6bfd2aa14a456a5d5eef414ee1678db615
Yukihiko Saito <y-saito@safie.jp>
2020-03-23 17:59:07 +0900

    APIドキュメントのセルフホスティング https://fastapi.tiangolo.com/advanced/extending-openapi/

--- main.py
+++ main.py
@@ -35,6 +35,12 @@ from fastapi.templating import Jinja2Templates
 
 # @see https://fastapi.tiangolo.com/advanced/extending-openapi/
 from fastapi.openapi.utils import get_openapi
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+
 
 from enum import Enum
 # @see https://fastapi.tiangolo.com/tutorial/body/
@@ -142,7 +148,7 @@ def fake_decode_token(token):
 
 
 elastic_apm = make_apm_client({})
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 app.add_middleware(ElasticAPM, client=elastic_apm)
 # app.add_middleware(HTTPSRedirectMiddleware)
 # app.add_middleware(
@@ -290,6 +296,31 @@ def custom_openapi():
 
 
 app.openapi = custom_openapi
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
 
 
 @app.middleware("http")
