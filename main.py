68bae7768c2c9c5a45078974571d210f86f40986
Yukihiko Saito <y-saito@safie.jp>
2020-03-23 13:01:15 +0900

    OAuth2 スコープ https://fastapi.tiangolo.com/advanced/security/oauth2-scopes/

--- main.py
+++ main.py
@@ -10,9 +10,14 @@
 # @see https://fastapi.tiangolo.com/tutorial/middleware/
 import time
 from fastapi import FastAPI, Query, Path, Body, Header, status, \
-    HTTPException, Depends, Request
+    HTTPException, Depends, Request, Security
 # @see https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+# @see https://fastapi.tiangolo.com/advanced/security/oauth2-scopes/
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm,
+    SecurityScopes,
+)
 # @see https://fastapi.tiangolo.com/tutorial/encoder/
 # @see https://fastapi.tiangolo.com/tutorial/cors/
 from fastapi.middleware.cors import CORSMiddleware
@@ -24,7 +29,8 @@ from fastapi.responses import JSONResponse, RedirectResponse, \
 from enum import Enum
 # @see https://fastapi.tiangolo.com/tutorial/body/
 # @see https://fastapi.tiangolo.com/tutorial/extra-models/
-from pydantic import BaseModel, HttpUrl, EmailStr
+# @see https://fastapi.tiangolo.com/advanced/security/oauth2-scopes/
+from pydantic import BaseModel, HttpUrl, EmailStr, ValidationError
 # @see https://fastapi.tiangolo.com/tutorial/body-nested-models/
 from typing import Optional, List, Set
 
@@ -98,6 +104,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str = None
+    scopes: List[str] = []
 
 
 def get_user(db, username: str):
@@ -156,7 +163,10 @@ app.add_middleware(
 )
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/token",
+    scopes={"me": "Read information about the current user.", "items": "Read items."},
+)
 
 
 def verify_password(plain_password, hashed_password):
@@ -188,28 +198,44 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
 
 
 # DBに該当ユーザーが存在しているかチェック
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(
+        security_scopes: SecurityScopes, token: str = Depends(oauth2_scheme)
+):
+    if security_scopes.scopes:
+        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
+    else:
+        authenticate_value = f"Bearer"
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
+        headers={"WWW-Authenticate": authenticate_value},
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
-    except PyJWTError:
+        token_scopes = payload.get("scopes", [])
+        token_data = TokenData(scopes=token_scopes, username=username)
+    except (PyJWTError, ValidationError):
         raise credentials_exception
     user = get_user(fake_users_db, username=token_data.username)
     if user is None:
         raise credentials_exception
+    for scope in security_scopes.scopes:
+        if scope not in token_data.scopes:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Not enough permissions",
+                headers={"WWW-Authenticate": authenticate_value},
+            )
     return user
 
 
 # DBの値 (disabled == True) ならば拒否する
-async def get_current_active_user(current_user: UserBase = Depends(get_current_user)):
+async def get_current_active_user(
+        current_user: UserBase = Security(get_current_user, scopes=["me"])
+):
     if current_user.disabled:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
     return current_user
@@ -240,7 +266,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user.username, "scopes": form_data.scopes},
+        expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
